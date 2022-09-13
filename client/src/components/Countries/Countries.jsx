3b796007d2be6b35e6328b9595b1d04ca23d0a50
TomCostabel <tomasperalta1997@hotmail.com>
2022-09-13 20:03:31 -0300

    commit pi

--- client/src/components/Countries/Countries.jsx
+++ client/src/components/Countries/Countries.jsx
@@ -1,20 +1,66 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllCountries } from "../../redux/actions/index.js";
 import Card from "../Card/Card";
 import "../Countries/Countries.css";
 import NavBar from "../NavBar/NavBar.jsx";
-
+import Pagination from "../Pagination/Pagination.jsx";
 export default function Countries() {
+    //-----------------------------MY STATES---------------------------------------------->
+
     const paises = useSelector((state) => state.countries);
 
+    const [currentPage, setCurrentPage] = useState(0);
+    const [datos, setDatos] = useState([]);
+    const [paisesActuales, setPaisesActuales] = useState([]);
+    //----------------------------------------
+    const ITEMS_X_PAGE = 10;
     const dispatch = useDispatch();
+    //-----------------------------USE EFFECTS------------------------------------------->
+
+    useEffect(() => {
+        if (paises.length && !datos.length) setDatos(paises);
+        if (!datos.length) dispatch(getAllCountries());
+        setPaisesActuales(
+            datos.slice(
+                currentPage * ITEMS_X_PAGE,
+                currentPage * ITEMS_X_PAGE + ITEMS_X_PAGE
+            )
+        );
+    }, [dispatch, currentPage, datos, paises]);
 
     useEffect(() => {
         dispatch(getAllCountries());
     }, [dispatch]);
+    //-----------------------------PREV AND NEXT HANDLER--------------------------------->
 
-    const allTheCountries = paises.map((e) => {
+    const nextHandler = () => {
+        const nextPage = currentPage + 1;
+
+        const firstIndex = nextPage * ITEMS_X_PAGE;
+
+        if (paisesActuales.length < 10) return;
+        setPaisesActuales(
+            [...datos].slice(firstIndex, firstIndex + ITEMS_X_PAGE)
+        );
+        setCurrentPage(nextPage);
+    };
+    const prevHandler = () => {
+        const prevPage = currentPage - 1;
+
+        if (prevPage < 0) return;
+
+        const firstIndex = prevPage * ITEMS_X_PAGE;
+
+        setPaisesActuales(
+            [...datos].slice(firstIndex, firstIndex + ITEMS_X_PAGE)
+        );
+        setCurrentPage(prevPage);
+    };
+
+    //------------------------------MAPEO PARA MOSTRAR LAS CARDS---------------------------------------------------->
+
+    const allTheCountries = paisesActuales.map((e) => {
         return (
             <Card
                 key={e.id}
@@ -29,7 +75,17 @@ export default function Countries() {
     return (
         <div>
             <NavBar />
-            <div className="container-cards">{allTheCountries}</div>
+            {/* <div className="container-cards">{allTheCountries}</div> */}
+            <div className="container-cards">
+                {
+                    <Pagination
+                        items={allTheCountries}
+                        currentPage={currentPage}
+                        nextHandler={nextHandler}
+                        prevHandler={prevHandler}
+                    />
+                }
+            </div>
         </div>
     );
 }
