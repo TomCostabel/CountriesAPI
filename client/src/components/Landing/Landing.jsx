8ad0e334bfa06351d262e6465e38879179f7b9f2
TomCostabel <tomasperalta1997@hotmail.com>
2022-09-13 18:36:39 -0300

    commit pi

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 export default function Landing() {
     return (
         <div>
-            <h1>SOY EL HOME</h1>
             <Link to="/Home">
                 <h1>HOME</h1>
             </Link>
